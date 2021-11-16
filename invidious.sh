74e608acc9933b98ad4ecd5fb6e49e80949057f1
Toasterbirb <git.53jvg@aleeas.com>
2021-11-16 14:50:45 +0200

    Switched from scraping to using the API

--- invidious.sh
+++ invidious.sh
@@ -1,3 +1,5 @@
+instance="invidious.snopyta.org"
+
 [[ "$1" == "--verbose" ]] && v="1" # Enable verbose mode
 
 function verbose {
@@ -11,37 +13,25 @@ verbose "Query: $query"
 [ -z $query ] && exit 0
 
 notify-send -u low -t 4000 "Invidious" "Searching..."
-website=$(curl -qs https://invidious.snopyta.org/search?q=$query)
+website=$(curl -qs https://${instance}/api/v1/search?q=${query})
 
 # Get video results
-results=$(echo "$website" | grep "<p>.*watch" | sed 's|<p><a href="|https://invidious.snopyta.org|g; s|">|;|g; s|</a></p>||g; s/^[[:space:]]*//g')
-
-# Clean up results in case of special chars
-results="$(echo "$results" | sed "s/&#39;/'/g")"
-
-verbose "Results:\n$results"
-
-# Get thumbnails
-#thumbnails=$(echo "$website" | grep "<img.*thumbnail" | sed '/?sqp=/d; s|<img.*src="|https://invidious.snopyta.org|g; s|"/>||g; s/^[[:space:]]*//g' | nl -s ";")
+results=$(jq '.[] | {title, videoId}' <<< "$website")
+video_titles=$(jq -r '.title' <<< "$results")
+verbose "Results:\n$video_titles"
 
 # Show results in rofi menu
-video_name=$(echo -e "$(echo "$results" | cut -d';' -f2)" | rofi -dmenu -l 10 -width 1280 -P "Results")
+video_name=$(rofi -dmenu -l 10 -wdith 1280 -P "Results" <<< $video_titles)
 verbose "Video name: $video_name"
 
 # Search for the link in results
 notify-send -u low -t 4000 "Invidious" "Opening video:
 $video_name"
 
-verbose "Removing illegal characters"
-video_name=$(echo "$video_name" | sed 's/\[//g; s/\]//g')
-results=$(echo "$results" | sed 's/\[//g; s/\]//g')
-
-verbose "Looking for the correct line"
-urlLine=$(echo "$results" | grep ";$video_name$")
-verbose "URL Line: $urlLine"
-url=$(echo "$urlLine" | cut -d';' -f1)
+videoID=$(jq -r "select(.title == \"${video_name}\").videoId" <<< $results)
+url="https://${instance}/watch?v=$videoID"
 
 verbose "Url: $url"
 
 # Open the video in mpv
-mpv --ytdl-format="bestvideo[height<=?1080][fps<=?30][vcodec!=?vp9]+bestaudio/best" $url
+mpv --ytdl-format="bestvideo[height<=?1080][fps<=?30][vcodec!=?vp9]+bestaudio/best" "$url"
